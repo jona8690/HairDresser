bd5697cff372ca9ab9fe4d8109b284cd749a9adf
Roxana Ion <ionroxana95@yahoo.com>
2017-02-06 13:48:22 +0100

    Add project files.

---
+++ HairDresser/HairDresser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairDresser
+{
+    class HairDresser
+    {
+        public Scissors left { get; set; }
+        public Scissors right { get; set; }
+
+        public HairDresser(Scissors left, Scissors right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ HairDresser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairDresser
+{
+    class Program
+    {
+        Scissors one = new Scissors();
+        Scissors two = new Scissors();
+        Scissors three = new Scissors();
+        Scissors four = new Scissors();
+        Scissors five = new Scissors();
+        Scissors six = new Scissors();
+
+        HairDresser A = new HairDresser();
+        HairDresser B = new HairDresser();
+        HairDresser C = new HairDresser();
+        HairDresser D = new HairDresser();
+        HairDresser E = new HairDresser();
+        HairDresser F = new HairDresser();
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+
+        }
+    }
+}
